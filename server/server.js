be30e5d75c86931c164f4170b9f9d87e3bc7cb3b
abhigyan-dwivedi <abhigyan.dwivedi@outlook.com>
2017-12-31 03:56:15 +0530

    Updated server

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
-const express=require('express');
-
-const path=require('path');
-
-const publicPath=path.join(__dirname,'../public')
-
+const path=require('path')
+const express =require('express');
 const port=process.env.PORT||3000;
+
+
+var publicPath=path.join(__dirname,'../public');
+console.log(publicPath);
 
 var app=express();
 
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
-    console.log('Server up on port:'+3000);    
-});
 
+app.listen(port,()=>{
+  console.log(`Server started listening on port ${port}`);
+});
