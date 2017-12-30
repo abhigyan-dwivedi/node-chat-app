7b89e71ba563f6b78054d9f03e9b94f57b2ced20
abhigyan-dwivedi <abhigyan.dwivedi@outlook.com>
2017-12-31 03:29:20 +0530

    Updated server

--- server/server.js
+++ server/server.js
@@ -4,11 +4,13 @@ const path=require('path');
 
 const publicPath=path.join(__dirname,'../public')
 
+const port=process.env.PORT||3000;
+
 var app=express();
 
 app.use(express.static(publicPath));
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log('Server up on port:'+3000);    
 });
 
